c57a3c2110c7ca47af1b95f6b2d43c08bb0697a7
ygribkov <yury.gribkov@gmail.com>
2014-03-28 15:20:41 +0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "chess-task"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.0" % "test"
---
+++ src/test/scala/xxx/chesstask/TestTest.scala
@@ -0,0 +1,10 @@
+package xxx.chesstask
+
+import org.scalatest._
+
+class ExampleSpec extends FlatSpec with Matchers {
+
+  "Dummy test" should "pass" in {
+    1 should be (1)
+  }
+}
