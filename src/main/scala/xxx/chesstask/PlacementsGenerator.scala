b81876030806891f61ddd977d8276490bcb2e169
ygribkov <yury.gribkov@gmail.com>
2014-03-30 11:24:47 +0400

    simplified PlacementsGenerator

--- src/main/scala/xxx/chesstask/PlacementsGenerator.scala
+++ src/main/scala/xxx/chesstask/PlacementsGenerator.scala
@@ -2,37 +2,32 @@ package xxx.chesstask
 
 object PlacementsGenerator {
 
-  def generateUniquePlacements(figures: List[Figure]): Set[List[Figure]] =
-    Symbols(figures).generateUniquePlacements
-
-  private object Symbols {
-    def apply(figures: Seq[Figure]): Symbols = {
-      val figureToNumber = figures.groupBy(identity) map {
-        case (k, v) => (k, v.size)
-      }
-      new Symbols(figureToNumber)
+  def apply(figures: Seq[Figure]): PlacementsGenerator = {
+    val figureToNumber = figures.groupBy(identity) map {
+      case (k, v) => (k, v.size)
     }
+    new PlacementsGenerator(figureToNumber)
   }
+}
 
-  private class Symbols(figureToNumber: Map[Figure, Int]) {
-    def generateUniquePlacements: Set[List[Figure]] =
-      uniqueSymbols match {
-        case s if s.isEmpty => Set(Nil)
-        case availableUniqueSymbols =>
-          for {
-            e <- availableUniqueSymbols
-            tail <- excludeOne(e).generateUniquePlacements
-          } yield e :: tail
-      }
-
-    def uniqueSymbols: Set[Figure] = figureToNumber.keySet
-
-    def excludeOne(f: Figure): Symbols = {
-      val number = figureToNumber(f)
-      new Symbols(
-        if (number > 1) figureToNumber.updated(f, number - 1)
-        else figureToNumber - f
-      )
+class PlacementsGenerator(figureToNumber: Map[Figure, Int]) {
+  def generateUniquePlacements: Set[List[Figure]] =
+    uniqueSymbols match {
+      case s if s.isEmpty => Set(Nil)
+      case availableUniqueSymbols =>
+        for {
+          e <- availableUniqueSymbols
+          tail <- excludeOne(e).generateUniquePlacements
+        } yield e :: tail
     }
+
+  def uniqueSymbols: Set[Figure] = figureToNumber.keySet
+
+  def excludeOne(f: Figure): PlacementsGenerator = {
+    val number = figureToNumber(f)
+    new PlacementsGenerator(
+      if (number > 1) figureToNumber.updated(f, number - 1)
+      else figureToNumber - f
+    )
   }
 }
--- src/test/scala/xxx/chesstask/PlacementsGeneratorSpec.scala
+++ src/test/scala/xxx/chesstask/PlacementsGeneratorSpec.scala
@@ -4,17 +4,16 @@ import org.scalatest._
 
 class PlacementsGeneratorSpec extends FlatSpec with Matchers {
 
-  import PlacementsGenerator._
   import Figure._
 
   "PlacementsGenerator" should "generateUniquePlacements" in {
-    generateUniquePlacements(List()) should be(Set(Nil))
-    generateUniquePlacements(List(K)) should be(Set(List(K)))
-    generateUniquePlacements(List(K, Q)) should be(Set(List(K, Q), List(Q, K)))
+    PlacementsGenerator(List()).generateUniquePlacements should be(Set(Nil))
+    PlacementsGenerator(List(K)).generateUniquePlacements should be(Set(List(K)))
+    PlacementsGenerator(List(K, Q)).generateUniquePlacements should be(Set(List(K, Q), List(Q, K)))
   }
 
   "PlacementsGenerator" should "be able to generate many placements" in {
-    val result = generateUniquePlacements(List(N, K, K, K, K, Q, B, R, N))
+    val result = PlacementsGenerator(List(N, K, K, K, K, Q, B, R, N)).generateUniquePlacements
     result.size should be(7560)
   }
 }
